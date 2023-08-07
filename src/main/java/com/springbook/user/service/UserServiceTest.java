00d0ae4a69e3a7716315ccd66fc4c94bb09e70e6
98kunwu <98kunwu@gmail.com>
2023-08-07 14:49:09 +0900

    다이내믹 프록시 적용 트랜잭션 테스트

--- src/main/java/com/springbook/user/service/UserServiceTest.java
+++ src/main/java/com/springbook/user/service/UserServiceTest.java
@@ -6,6 +6,7 @@ import static com.springbook.user.service.UserServiceImpl.MIN_RECCOMEND_FOR_GOLD
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
+import java.lang.reflect.Proxy;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -182,9 +183,12 @@ public class UserServiceTest {
         testUserService.setUserDao(userDao);
         testUserService.setMailSender(mailSender);
 
-        UserServiceTx txUserService = new UserServiceTx();
-        txUserService.setTransactionManager(transactionManager);
-        txUserService.setUserService(testUserService);
+        TransactionHandler txHandler = new TransactionHandler();
+        txHandler.setTarget(testUserService);
+        txHandler.setTransactionManager(transactionManager);
+        txHandler.setPattern("upgradeLevels");
+        UserService txUserService = (UserService)Proxy.newProxyInstance(
+                getClass().getClassLoader(), new Class[] {UserService.class}, txHandler);
 
         userDao.deleteAll();
         for(User user : users) userDao.add(user);
